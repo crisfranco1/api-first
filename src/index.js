4e96b60fc4d4383cf2ec17ba5f35d0bed9e248ac
Cristian David Franco Garcia <cristiandavidfrancogarcia@gmail.com>
2025-07-28 12:09:05 +0200

    feature: Create user endpoint added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const OpenApiValidator = require('express-openapi-validator');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const swaggerDocument = YAML.load('./openapi.yaml');
 app.use('/docs', swagerUi.serve, swagerUi.setup(swaggerDocument));
 app.use(OpenApiValidator.middleware({
@@ -24,6 +26,17 @@ app.use((err, req, res, next) => {
 
 app.get('/hello', (req, res) => {
     res.json({ message: 'Hello World' });
+});
+
+app.post('/users', (req, res) => {
+    const { name, age, email } = req.body;
+    const newUser = {
+        id: Date.now().toString(),
+        name,
+        age,
+        email
+    };
+    res.status(201).json(newUser);
 });
 
 app.listen(port, () => {
